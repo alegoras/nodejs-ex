7268fb6ed1e67ff42b66095cf27e183a780b4fc4
Alex Guerra <alegoras@gmail.com>
2016-12-14 01:46:37 -0600

    uri

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(morgan('combined'))
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
-    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
+    mongoURL = "mongodb://<dbuser>:<dbpassword>@ds133338.mlab.com:33338/alcoholapp",//process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
     mongoURLLabel = "";
 
 if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
